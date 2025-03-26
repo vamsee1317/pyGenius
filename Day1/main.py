ebf516ba08061b815ef8027c056c10f77cfb110d
vamsee1317 <vamsee.achieversit@gmail.com>
2025-03-26 18:17:14 +0530

    Day1 - Python

---
+++ Day1/main.py
@@ -0,0 +1,10 @@
+print("HellooWorld!!!!")
+
+# check the user age to vote
+
+age = int(input("Enter your age"))
+
+if age >= 18:
+    print("You're eligible")
+else:
+    print("You're not eligible")
---
+++ Day1/theory.py
@@ -0,0 +1,48 @@
+# What is Python ?
+# Python is a high level, interpretted langauge.
+# It is easy to learn and use.
+
+# Why ?
+
+# Web Development - Django, Flask
+# Data Science - NumPy, Pandas, Matplotlib
+# Automation - Robot Process, web scrapping, automation scripts
+# Machine Learning - TensorFlow, Keras, Scikit-learn
+# Mobile App Development - Kivy, Buildozer
+
+# Tech Companies or software companies :
+
+# Google : uses for AI, search Algorith and YouTube
+# Facebook(Meta) : uses python for backend development and AI search.
+# Instagram : uses python for backend development.
+
+
+# First program
+# How to run 
+    # -- python fileName.py  (windows)
+    # -- python3 fileName.py (macos)
+
+#   download extensions
+    # code runner
+
+
+# Note Points :
+# Python is case sensitive.
+# Python is dynamically typed.
+# Python is object oriented.
+# Python is high level language.
+# Python is easy to learn and use.
+# Python is fast and efficient.
+# Python is widely used.
+# Python is open source.
+
+
+# Drawbacks :
+# Python is slow compared to C++.
+# Python is not suitable for game development.
+# Python is not suitable for mobile app development.
+# Python is not suitable for embedded systems.
+# Python is not suitable for high performance computing.
+
+
+# intendation
