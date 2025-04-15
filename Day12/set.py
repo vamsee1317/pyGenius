18acc172dd219637db731bd3f6409d234fc9028c
vamsee1317 <vamsee.achieversit@gmail.com>
2025-04-15 17:57:04 +0530

    Set | Methods

--- Day12/set.py
+++ Day12/set.py
@@ -49,8 +49,8 @@ print(set3)
 # difference :
 set1 = {1, 2, 3, 4, 5}
 set2 = {4, 5, 6, 7, 8}
-set3 = set1.difference(set2)
-set4 = set2.difference(set1)
+set3 = set1.symmetric_difference(set2)  # 1 2 3
+set4 = set2.difference(set1) # 8 6 7
 print(set3)
 print(set4)
 
